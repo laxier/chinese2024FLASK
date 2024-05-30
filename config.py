6aa797a04ee0e9227529d53ef083fa86d2b3da77
laxier <hash0xcb8c@yandex.ru>
2024-05-30 22:19:29 +0300

    бд в папке

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'chinese_app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'chinese_app.db')
     # SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:rootroot@localhost/chinese'
     # SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:rootroot@host.docker.internal/chinese'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://{username}:{password}@{host}/{database_name}'.format(
-        username=os.environ.get('USER'),
-        password=os.environ.get('PASSWORD'),
-        host='amvera-hahshkj2wkj-cnpg-chinese-flask-db-rw',
-        database_name=os.environ.get('DBNAME')
-    )
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://{username}:{password}@{host}/{database_name}'.format(
+    #     username=os.environ.get('USER'),
+    #     password=os.environ.get('PASSWORD'),
+    #     host='amvera-hahshkj2wkj-cnpg-chinese-flask-db-rw',
+    #     database_name=os.environ.get('DBNAME')
+    # )
     TIMEZONE = 'UTC'
