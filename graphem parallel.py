42d6e60d2f3aa02a34981b954f7976f63853b019
laxier <hash0xcb8c@yandex.ru>
2024-06-05 18:34:16 +0300

    Revert ""гуаминг" в названиях, в преводе убирается"
    This reverts commit bfd9ca94596581a586ecde77255880c4c0ebb024.

--- graphem parallel.py
+++ graphem parallel.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 # requires selenium==4.20.0
 MAX_THREADS = 5
 semaphore = threading.Semaphore(MAX_THREADS)
-id = 24
+id = 16
 
 def process_element(element):
     with app.app_context():
